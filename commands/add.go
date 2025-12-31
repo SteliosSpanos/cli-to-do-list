e48406e8f05d963c85a9d6108de26ce96bda6775
SteliosSpanos <stylspan19@gmail.com>
2025-12-31 12:42:56 +0200

    Adds the 'add' CLI command

---
+++ commands/add.go
@@ -0,0 +1,36 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/SteliosSpanos/cli-to-do-list/pkg"
+)
+
+const filename = "notes.json"
+
+func Add(args []string) {
+	if len(args) != 1 {
+		fmt.Println("Error: Please provide a description for the task")
+		fmt.Println("Usage: ./main add <description>")
+		os.Exit(1)
+	}
+
+	description := args[0]
+
+	taskList, err := pkg.ReadFromFile(filename)
+	if err != nil {
+		fmt.Printf("Error reading notes: %v\n", err)
+		os.Exit(1)
+	}
+
+	taskList.AddTask(description)
+
+	err = pkg.SaveToFile(filename, taskList)
+	if err != nil {
+		fmt.Printf("Error saving tasks: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Task Added Successfully!")
+}
