fc7c921b3276fd1776131b6e8c9a62ecd40f336d
SteliosSpanos <stylspan19@gmail.com>
2025-12-31 12:53:27 +0200

    Changes 'options' to 'arguments'

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	if len(os.Args) == 1 {
-		fmt.Println("Usage: ./main <command> [options]")
+		fmt.Println("Usage: ./main <command> [arguments]")
 		fmt.Println("    add         add task in the list")
 		fmt.Println("    delete      delete a task from the list")
 		fmt.Println("    complete    set a task as completed")
